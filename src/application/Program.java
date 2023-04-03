4c6568d6a4c05f62c8a1982180c46692759165f5
Gabriel Muniz Lopes <oficial.gabrielmlopes@gmail.com>
2023-04-03 19:49:05 -0300

    Delete Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== TEST 1: SELLER 'findById' ===");
@@ -31,15 +34,23 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		/*
 		System.out.println("\n=== TEST 4: SELLER 'INSERT' ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
-		
+		*/
 		System.out.println("\n=== TEST 5: SELLER 'UPDATE' ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update Completed!");
+		
+		System.out.println("\n=== TEST 6: SELLER 'DELETE' ===");
+		System.out.println("Enter ID for delete test: ");
+		sellerDao.deleteById(scan.nextInt());
+		System.out.println("Delete Completed!");
+		
+		scan.close();
 	}
 }
